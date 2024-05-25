eb827e65fd1405ee17538298d4f034b857629b47
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-05-25 12:50:16 -0500

    add: configurar react router con navegación

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,6 +1,11 @@
+import { AppRouter } from "./assets/router/AppRouter"
 
 export const HeroesApp = () => {
     return (
-        <h1>HeroesApp</h1>
+        <>
+            <h1>HeroesApp</h1>
+            <AppRouter />
+        </>
+
     )
 }
