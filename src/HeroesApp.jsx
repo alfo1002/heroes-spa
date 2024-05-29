0aa6d4ac073e77bb67de4cb65d11b3794baf2036
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-05-28 20:01:16 -0500

    add: usando useNavigate

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -3,7 +3,6 @@ import { AppRouter } from "./assets/router/AppRouter"
 export const HeroesApp = () => {
     return (
         <>
-            <h1>HeroesApp</h1>
             <AppRouter />
         </>
 
