8a4ecbf31264ea1d3d59dcd22ef1c71b3db4bafe
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-05-28 21:16:56 -0500

    add: agregando useMemo para almacenar valores(id) y detectar cambios para actualizaciones

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,11 +1,12 @@
 import { Navigate, useParams } from "react-router-dom"
 import { getHeroesById } from "../helpers/getHeroesById"
+import { useMemo } from "react"
 
 export const HeroPage = () => {
 
     const { id } = useParams()
 
-    const hero = getHeroesById(id)
+    const hero = useMemo(() => getHeroesById(id), [id])
 
     if (!hero) {
         return <Navigate to={"/marvel"} />
