c9d8a3323e3ef80677bc0c4caf5ea1f226cdbf75
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-05-29 15:16:31 -0500

    add: implementar logica para rutas privadas

--- src/assets/router/AppRouter.jsx
+++ src/assets/router/AppRouter.jsx
@@ -5,18 +5,29 @@ import { LoginPage } from "../../auth/pages/LoginPage"
 import { Navbar } from "../../ui/components/Navbar"
 import { HeroPage } from "../../heroes/pages/HeroPage"
 import { SearchPage } from "../../heroes/pages/SearchPage"
+import { PrivateRoute } from "./PrivateRoute"
 
 export const AppRouter = () => {
     return (
         <>
             <Navbar />
             <Routes>
-                <Route path="marvel" element={<MarvelPage />} />
-                <Route path="dc" element={<DcPage />} />
-                <Route path="hero/:id" element={<HeroPage />} />
-                <Route path="search" element={<SearchPage />} />
-                <Route path="login" element={<LoginPage />} />
-                <Route path="/" element={<Navigate to="/marvel" />} />
+
+                <Route path="/login" element={<LoginPage />} />
+
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <Routes>
+                            <Route path="marvel" element={<MarvelPage />} />
+                            <Route path="dc" element={<DcPage />} />
+                            <Route path="hero/:id" element={<HeroPage />} />
+                            <Route path="search" element={<SearchPage />} />
+                            <Route path="*" element={<Navigate to="/marvel" />} />
+                        </Routes>
+                    </PrivateRoute>
+                } />
+
+                {/* <Route path="/" element={<Navigate to="/marvel" />} /> */}
             </Routes>
         </>
     )
